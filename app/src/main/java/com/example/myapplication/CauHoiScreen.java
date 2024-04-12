0cf3c3e84cb10af652a0956de2e2cb8213e8e286
Onion <vuongha16104@gmail.com>
2024-04-12 13:42:25 +0700

    finsh mainscreen

--- app/src/main/java/com/example/myapplication/CauHoiScreen.java
+++ app/src/main/java/com/example/myapplication/CauHoiScreen.java
@@ -8,18 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainScreen extends AppCompatActivity {
+public class CauHoiScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main_screen);
-        try
-        {
-            this.getSupportActionBar().hide();
-        }
-        catch (NullPointerException e){}
+        setContentView(R.layout.activity_cau_hoi_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
