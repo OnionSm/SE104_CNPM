8e6e9e9275825e575809f2a2514e949a74a5af1e
OnionSm <vuongha16104@gmail.com>
2024-05-16 13:51:16 +0700

    up animation taodethi

---
+++ app/src/main/java/my_interface/IPassingData.java
@@ -0,0 +1,10 @@
+package my_interface;
+
+import com.example.myapplication.taodethicauhoiitem;
+
+import java.util.ArrayList;
+
+public interface IPassingData
+{
+    void PassData(ArrayList<taodethicauhoiitem> list_cau_hoi);
+}
